951a72f01defb27c49e9dc2fc65e942bd2fa93a9
Yan Sagica <yanssf4@gmail.com>
2022-04-01 17:52:35 -0300

    desenvolvendo aprovado e reprovado

--- src/pages/SecAprovReprov/index.js
+++ src/pages/SecAprovReprov/index.js
@@ -1,92 +1,183 @@
-import React, { useState } from 'react'
-import Footer from '../../components/Footer'
-import { FaUsers, FaUserFriends, FaCarAlt, FaDog } from 'react-icons/fa';
-import { MdOutlineFreeCancellation, MdRestore } from 'react-icons/md';
-import { BiTransfer } from 'react-icons/bi';
-import {AiOutlineLike, AiOutlineFileExclamation, AiOutlineDislike} from 'react-icons/ai'
-import '../SecAprovReprov/style.css'
+import React, { useEffect, useState } from "react";
+import Footer from "../../components/Footer";
+import { FaUsers, FaUserFriends, FaCarAlt, FaDog } from "react-icons/fa";
+import { MdOutlineFreeCancellation, MdRestore } from "react-icons/md";
+import { BiTransfer } from "react-icons/bi";
+import {
+  AiOutlineLike,
+  AiOutlineFileExclamation,
+  AiOutlineDislike,
+} from "react-icons/ai";
+import "../SecAprovReprov/style.css";
+import Pizza from "../../components/Graficos/Pizza";
+import axios from "axios";
+
+var cnpj = "83369678000173";
 
 export default function SecAprovReprov() {
+  const [qtdMatriculados, setQtdMatriculados] = useState([]);
+  const [qtdFichados, setQtdFichados] = useState([]);
+  const [qtdEvasao, setQtdEvasao] = useState([]);
+  const [qtdTfa, setQtdTfa] = useState([]);
+  const [registros, setRegistros] = useState([]);
+  const [anoPeriodo, setAnoPeriodo] = useState("");
+  const [periodos, setPeriodos] = useState([]);
+  const [seqano, setSeqano] = useState("");
+  const [turma, setTurma] = useState("");
+  const [turmas, setTurmas] = useState([]);
 
-   return (
-      <>
-         <div className="bg-light" id="layoutSidenav_content">
-            <main>
-               <div className="container-fluid px-4">
-                  <h3 className="mt-4 mb-4 fw-bold texto-roxo">Aprovados/Reprovados</h3>
-                  <hr />
-                  <div class="container">
-                     <div className='row'>
-                        <div className='col-2'>
-                           <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                              <option selected value="1">2020/1</option>
-                              <option value="2">2021/1</option>
-                              <option value="3">2022/1</option>
-                           </select>
-                        </div>
-                        <div className='col-10'>
-                           <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                              <option selected value="1">Selecione a turma</option>
-                              <option value="2">1AM</option>
-                              <option value="3">2AM</option>
-                           </select>
-                        </div>
-                     </div>
-                     <div class="row">
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-green order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Aprovados</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <AiOutlineLike size="55px" />
-                                    <span className="h1 fw-bold">351</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  const getTodosPeriodos = async () => {
+    const response = await axios.get(
+      `http://localhost:5000/secretaria/periodos/${cnpj}`
+    );
+    setPeriodos(response.data);
+  };
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-blue order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Aprovados com dependência</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <AiOutlineFileExclamation color="" size="55px" />
-                                    <span className="h1 fw-bold">20</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  const getTurmas = async () => {
+    const periodo = anoPeriodo.split("/");
+    const response = await axios.get(
+      `http://localhost:5000/secretaria/turmas/${cnpj}/${periodo[0]}/${periodo[1]} `
+    );
+    setTurmas(response.data);
+  };
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-orange order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Recuperação</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <MdRestore color="" size="55px" />
-                                    <span className="h1 fw-bold">10</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  useEffect(() => {
+    getTodosPeriodos();
+    getTotalRegistros();
+  }, []);
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-red order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Reprovados</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <AiOutlineDislike color="" size="55px" />
-                                    <span className="h1 fw-bold">2</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+  useEffect(() => {
+    if (anoPeriodo.length > 0) {
+      getTurmas();
+    }
+    getTotalRegistros();
+  }, [anoPeriodo]);
+
+  useEffect(() => {
+    if (turma.length > 0) {
+      getTotalRegistros();
+    }
+  }, [turma]);
+
+  const getTotalRegistros = async () => {
+    let url = `http://localhost:5000/secretaria/${cnpj}`;
+    const periodo = anoPeriodo.split("/");
+
+    if (anoPeriodo.length > 0)
+      url = `http://localhost:5000/secretaria/${cnpj}/${periodo[0]}/${periodo[1]}`;
+
+    if (turma.length > 0)
+      url = `http://localhost:5000/secretaria/turmas/${cnpj}/${periodo[0]}/${periodo[1]}/${turma}`;
+
+    const response = await axios.get(url);
+    const allData = response.data;
+    console.log(allData);
+    setRegistros(allData);
+
+    const totalMatric = allData.reduce((total, item) => total + item.qtdmat, 0);
+    const totalFic = allData.reduce((total, item) => total + item.qtdfic, 0);
+    const totalEva = allData.reduce((total, item) => total + item.qtdcats, 0);
+    const totalTfa = allData.reduce((total, item) => total + item.qtdtfa, 0);
+
+    setQtdMatriculados(totalMatric);
+    setQtdFichados(totalFic);
+    setQtdEvasao(totalEva);
+    setQtdTfa(totalTfa);
+  };
+  //   setUserData({
+  //     labels: ["Recebidos", "Vencidos"],
+  //     datasets: [
+  //       {
+  //         label: "userGain",
+  //         data: [
+  //           totalRec01 +
+  //             totalRec02 +
+  //             totalRec03 +
+  //             totalRec04 +
+  //             totalRec05 +
+  //             totalRec06 +
+  //             totalRec07 +
+  //             totalRec08 +
+  //             totalRec09 +
+  //             totalRec10 +
+  //             totalRec11 +
+  //             totalRec12,
+
+  //           totalDeb01 +
+  //             totalDeb02 +
+  //             totalDeb03 +
+  //             totalDeb04 +
+  //             totalDeb05 +
+  //             totalDeb06 +
+  //             totalDeb07 +
+  //             totalDeb08 +
+  //             totalDeb09 +
+  //             totalDeb10 +
+  //             totalDeb11 +
+  //             totalDeb12,
+  //         ],
+  //         backgroundColor: ["rgba(75,192,192,1)", "#FF0000"],
+  //       },
+  //     ],
+  //   });
+
+  return (
+    <>
+      <div className="bg-light" id="layoutSidenav_content">
+        <main>
+          <div className="container-fluid px-4">
+            <h3 className="mt-4 mb-4 fw-bold texto-roxo">
+              Aprovados/Reprovados
+            </h3>
+            <hr />
+            <div className="container">
+              <div className="row">
+                <div className="col-2">
+                  <select
+                    className="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    value={anoPeriodo}
+                    onChange={(e) => setAnoPeriodo(e.target.value)}
+                  >
+                    <option value="">...</option>
+                    {periodos.map((p, index) => (
+                      <option key={index} value={p.ano + "/" + p.seqano}>
+                        {p.ano}/{p.seqano}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="col-10">
+                  <select
+                    className="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    value={turma}
+                    onChange={(e) => setTurma(e.target.value)}
+                  >
+                    <option value="">...</option>
+                    {turmas.map((t, index) => (
+                      <option key={index} value={t.turma}>
+                        {t.turma}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="row">
+                {/* <div className="col-md-4 col-xl-3">
+                  <h5 className="fw-bold">Mensalidades</h5>
+                  <div className="card bg-c-light order-card">
+                    <div className="card-block">
+                      {userData && <Pizza chartData={userData} />}
+                    </div>
                   </div>
-               </div>
-            </main>
-            <Footer />
-         </div>
-      </>
-   )
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 }
-
--- src/pages/SecEvasaoTurma/index.js
+++ src/pages/SecEvasaoTurma/index.js
@@ -1,91 +1,187 @@
-import React, { useState } from 'react'
-import Footer from '../../components/Footer'
-import { FaUsers, FaUserFriends, FaCarAlt, FaDog } from 'react-icons/fa';
-import { MdOutlineFreeCancellation } from 'react-icons/md';
-import { BiTransfer } from 'react-icons/bi';
-import '../SecEvasaoTurma/style.css'
+import React, { useState, useEffect } from "react";
+import Footer from "../../components/Footer";
+import { FaUsers, FaUserFriends, FaCarAlt, FaDog } from "react-icons/fa";
+import { MdOutlineFreeCancellation } from "react-icons/md";
+import { BiTransfer } from "react-icons/bi";
+import "../SecEvasaoTurma/style.css";
+import axios from "axios";
+
+var cnpj = "83369678000173";
 
 export default function SecEvasaoTurma() {
+  const [qtdMatriculados, setQtdMatriculados] = useState([]);
+  const [qtdFichados, setQtdFichados] = useState([]);
+  const [qtdEvasao, setQtdEvasao] = useState([]);
+  const [qtdTfa, setQtdTfa] = useState([]);
+  const [registros, setRegistros] = useState([]);
+  const [anoPeriodo, setAnoPeriodo] = useState("");
+  const [periodos, setPeriodos] = useState([]);
+  const [seqano, setSeqano] = useState("");
+  const [turma, setTurma] = useState("");
+  const [turmas, setTurmas] = useState([]);
 
-   return (
-      <>
-         <div className="bg-light" id="layoutSidenav_content">
-            <main>
-               <div className="container-fluid px-4">
-                  <h3 className="mt-4 mb-4 fw-bold texto-roxo">Matriculados x Evasão (Turma)</h3>
-                  <hr />
-                  <div class="container">
-                     <div className='row'>
-                        <div className='col-2'>
-                           <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                              <option selected value="1">2020/1</option>
-                              <option value="2">2021/1</option>
-                              <option value="3">2022/1</option>
-                           </select>
-                        </div>
-                        <div className='col-10'>
-                           <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                              <option selected value="1">Selecione a turma</option>
-                              <option value="2">1AM</option>
-                              <option value="3">2AM</option>
-                           </select>
-                        </div>
-                     </div>
-                     <div class="row">
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-green order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Matriculados</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <FaUsers size="55px" />
-                                    <span className="h1 fw-bold">351</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  const getTodosPeriodos = async () => {
+    const response = await axios.get(
+      `http://localhost:5000/secretaria/periodos/${cnpj}`
+    );
+    setPeriodos(response.data);
+  };
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-blue order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Fichados (Registrados)</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <FaUserFriends color="" size="55px" />
-                                    <span className="h1 fw-bold">20</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  const getTurmas = async () => {
+    const periodo = anoPeriodo.split("/");
+    const response = await axios.get(
+      `http://localhost:5000/secretaria/turmas/${cnpj}/${periodo[0]}/${periodo[1]} `
+    );
+    setTurmas(response.data);
+  };
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-red order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">Evasão</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <MdOutlineFreeCancellation color="" size="55px" />
-                                    <span className="h1 fw-bold">10</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+  useEffect(() => {
+    getTodosPeriodos();
+    getTotalRegistros();
+  }, []);
 
-                        <div class="col-md-4 col-xl-3">
-                           <div class="card bg-c-orange order-card">
-                              <div class="card-block">
-                                 <h6 class="m-b-20 fw-bold text-center">TFA (Tranferência Final de Ano)</h6>
-                                 <div className="d-flex align-items-center justify-content-around">
-                                    <BiTransfer color="" size="55px" />
-                                    <span className="h1 fw-bold">2</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+  useEffect(() => {
+    if (anoPeriodo.length > 0) {
+      getTurmas();
+    }
+    getTotalRegistros();
+  }, [anoPeriodo]);
+
+  useEffect(() => {
+    if (turma.length > 0) {
+      getTotalRegistros();
+    }
+  }, [turma]);
+
+  const getTotalRegistros = async () => {
+    let url = `http://localhost:5000/secretaria/${cnpj}`;
+    const periodo = anoPeriodo.split("/");
+
+    if (anoPeriodo.length > 0)
+      url = `http://localhost:5000/secretaria/${cnpj}/${periodo[0]}/${periodo[1]}`;
+
+    if (turma.length > 0)
+      url = `http://localhost:5000/secretaria/turmas/${cnpj}/${periodo[0]}/${periodo[1]}/${turma}`;
+
+    const response = await axios.get(url);
+    const allData = response.data;
+    console.log(allData);
+    setRegistros(allData);
+
+    const totalMatric = allData.reduce((total, item) => total + item.qtdmat, 0);
+    const totalFic = allData.reduce((total, item) => total + item.qtdfic, 0);
+    const totalEva = allData.reduce((total, item) => total + item.qtdcats, 0);
+    const totalTfa = allData.reduce((total, item) => total + item.qtdtfa, 0);
+
+    setQtdMatriculados(totalMatric);
+    setQtdFichados(totalFic);
+    setQtdEvasao(totalEva);
+    setQtdTfa(totalTfa);
+  };
+
+  return (
+    <>
+      <div className="bg-light" id="layoutSidenav_content">
+        <main>
+          <div className="container-fluid px-4">
+            <h3 className="mt-4 mb-4 fw-bold texto-roxo">
+              Matriculados x Evasão (Turma)
+            </h3>
+            <hr />
+            <div className="container">
+              <div className="row">
+                <div className="col-3">
+                  <select
+                    className="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    value={anoPeriodo}
+                    onChange={(e) => setAnoPeriodo(e.target.value)}
+                  >
+                    <option value="">...</option>
+                    {periodos.map((p, index) => (
+                      <option key={index} value={p.ano + "/" + p.seqano}>
+                        {p.ano}/{p.seqano}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="col-9">
+                  <select
+                    className="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    value={turma}
+                    onChange={(e) => setTurma(e.target.value)}
+                  >
+                    <option value="">...</option>
+                    {turmas.map((t, index) => (
+                      <option key={index} value={t.turma}>
+                        {t.turma}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 col-xl-3">
+                  <div className="card bg-c-green order-card">
+                    <div className="card-block">
+                      <h6 className="m-b-20 fw-bold text-center">
+                        Matriculados
+                      </h6>
+                      <div className="d-flex align-items-center justify-content-around">
+                        <FaUsers size="55px" />
+                        <span className="h1 fw-bold">{qtdMatriculados}</span>
+                      </div>
+                    </div>
                   </div>
-               </div>
-            </main>
-            <Footer />
-         </div>
-      </>
-   )
-}
+                </div>
 
+                <div className="col-md-4 col-xl-3">
+                  <div className="card bg-c-blue order-card">
+                    <div className="card-block">
+                      <h6 className="m-b-20 fw-bold text-center">
+                        Fichados (Registrados)
+                      </h6>
+                      <div className="d-flex align-items-center justify-content-around">
+                        <FaUserFriends color="" size="55px" />
+                        <span className="h1 fw-bold">{qtdFichados}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-4 col-xl-3">
+                  <div className="card bg-c-red order-card">
+                    <div className="card-block">
+                      <h6 className="m-b-20 fw-bold text-center">Evasão</h6>
+                      <div className="d-flex align-items-center justify-content-around">
+                        <MdOutlineFreeCancellation color="" size="55px" />
+                        <span className="h1 fw-bold">{qtdEvasao}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-4 col-xl-3">
+                  <div className="card bg-c-orange order-card">
+                    <div className="card-block">
+                      <h6 className="m-b-20 fw-bold text-center">
+                        Tranferência Final de Ano
+                      </h6>
+                      <div className="d-flex align-items-center justify-content-around">
+                        <BiTransfer color="" size="55px" />
+                        <span className="h1 fw-bold">{qtdTfa}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
