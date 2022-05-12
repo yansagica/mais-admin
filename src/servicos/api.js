3912d1ad5379e2d0cbfa53ace5c64d7f51e30979
Rodrigo Lopes <rlopeslameira@gmail.com>
2022-05-12 11:03:55 -0300

    api url

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://192.168.100.75:5000/",
+  // baseURL: "http://192.168.100.75:5000/",
+  baseURL: 'https://maisadmin.herokuapp.com/',
 });
 
 api.interceptors.request.use(
