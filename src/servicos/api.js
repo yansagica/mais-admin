f9508867ac9a6a03acd45858a7b52053029a0beb
Yan Sagica <yanssf4@gmail.com>
2022-06-24 08:12:39 -0300

    rota alterada

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  // baseURL: "https://maisadmin.herokuapp.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://maisadmin.herokuapp.com/",
+  // baseURL: "http://localhost:5000/",
 });
 
 api.interceptors.request.use(
