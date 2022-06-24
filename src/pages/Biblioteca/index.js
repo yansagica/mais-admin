e76a8c7d2892fd39fb604e83f3129bc14b41936c
Yan Sagica <yanssf4@gmail.com>
2022-06-24 16:23:27 -0300

    cards mais lidos e alunos que mais emprestam

--- src/pages/Biblioteca/index.js
+++ src/pages/Biblioteca/index.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../../components/Footer";
-import { FaUsers, FaUserFriends, FaMedal } from "react-icons/fa";
-import { MdOutlineFreeCancellation } from "react-icons/md";
-import { BiTransfer } from "react-icons/bi";
+
 import { BsBook } from "react-icons/bs";
 import { ImBooks } from "react-icons/im";
 import { GiPapers } from "react-icons/gi";
-import { BsJournalBookmark, BsTrophy } from "react-icons/bs";
-import { BiBookReader, BiBookHeart } from "react-icons/bi";
+
 import "../SecEvasaoCurso/style.css";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
@@ -20,6 +17,16 @@ export default function Biblioteca() {
   const [qtdObrasExe, setQtdObrasExe] = useState([]);
   const [qtdPeriodicos, setQtdPeriodicos] = useState([]);
   const [qtdPeriodicosExe, setQtdPeriodicosExe] = useState([]);
+  const [obrasMaisEmp1, setObrasMaisEmp1] = useState([]);
+  const [obrasMaisEmp2, setObrasMaisEmp2] = useState([]);
+  const [obrasMaisEmp3, setObrasMaisEmp3] = useState([]);
+  const [obrasMaisEmp4, setObrasMaisEmp4] = useState([]);
+  const [obrasMaisEmp5, setObrasMaisEmp5] = useState([]);
+  const [alunosMaisEmp1, setAlunosMaisEmp1] = useState([]);
+  const [alunosMaisEmp2, setAlunosMaisEmp2] = useState([]);
+  const [alunosMaisEmp3, setAlunosMaisEmp3] = useState([]);
+  const [alunosMaisEmp4, setAlunosMaisEmp4] = useState([]);
+  const [alunosMaisEmp5, setAlunosMaisEmp5] = useState([]);
 
   const cnpj = getCnpj();
 
@@ -29,12 +36,27 @@ export default function Biblioteca() {
 
   const getTodosRegistros = async () => {
     const resp = await api.get(`bib/${cnpj}`);
-    const allDados = resp.data;
+    const allData = resp.data;
 
-    setQtdObras(resp.data[0].qtdobr);
-    setQtdObrasExe(resp.data[0].qtdobrexe);
-    setQtdPeriodicos(resp.data[0].qtdper);
-    setQtdPeriodicosExe(resp.data[0].qtdperexe);
+    // ESTATISTICA DE OBRAS
+    setQtdObras(allData[0].qtdobr);
+    setQtdObrasExe(allData[0].qtdobrexe);
+    setQtdPeriodicos(allData[0].qtdper);
+    setQtdPeriodicosExe(allData[0].qtdperexe);
+
+    // 5 MAIS LIDAS
+    setObrasMaisEmp1(allData[0].obramaisemp1);
+    setObrasMaisEmp2(allData[0].obramaisemp2);
+    setObrasMaisEmp3(allData[0].obramaisemp3);
+    setObrasMaisEmp4(allData[0].obramaisemp4);
+    setObrasMaisEmp5(allData[0].obramaisemp5);
+
+    // 5 ALUNOS QUE MAIS EMPRESTAM
+    setAlunosMaisEmp1(allData[0].alumaisemp1);
+    setAlunosMaisEmp2(allData[0].alumaisemp2);
+    setAlunosMaisEmp3(allData[0].alumaisemp3);
+    setAlunosMaisEmp4(allData[0].alumaisemp4);
+    setAlunosMaisEmp5(allData[0].alumaisemp5);
   };
 
   return (
@@ -109,7 +131,7 @@ export default function Biblioteca() {
                     </div>
                   </div>
 
-                  <div className="row">
+                  {/* <div className="row">
                     <div className="col-md-4 col-xl-6">
                       <div className="card bg-c-green order-card">
                         <div className="card-block">
@@ -122,9 +144,9 @@ export default function Biblioteca() {
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
 
-                    <div className="col-md-4 col-xl-6">
+                  {/* <div className="col-md-4 col-xl-6">
                       <div className="card bg-c-blue order-card">
                         <div className="card-block">
                           <h6 className="m-b-20 fw-bold text-center">
@@ -136,33 +158,39 @@ export default function Biblioteca() {
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
+                    </div> */}
+                  {/* </div> */}
                 </div>
                 <h3 className="mt-4 mb-4 fw-bold texto-roxo">Top 5</h3>
                 <hr />
                 <div className="container">
                   <div className="row">
                     <div className="col-md-4 col-xl-6">
-                      <div className="card bg-c-yellow order-card">
+                      <div
+                        className="card order-card"
+                        style={{ backgroundColor: "#7998cd" }}
+                      >
                         <div className="card-block">
                           <div className="d-flex align-items-center justify-content-around">
-                            <BsTrophy size="55px" />
+                            {/* <BsTrophy size="55px" /> */}
                             <div className="d-flex flex-column justify-content-center">
                               <span className="h2 fw-bold">
                                 Os mais emprestados
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {obrasMaisEmp1}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {obrasMaisEmp2}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {obrasMaisEmp3}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {obrasMaisEmp4}
+                              </span>
+                              <span className="small fw-bold">
+                                {obrasMaisEmp5}
                               </span>
                             </div>
                           </div>
@@ -170,25 +198,31 @@ export default function Biblioteca() {
                       </div>
                     </div>
                     <div className="col-md-4 col-xl-6">
-                      <div className="card bg-c-yellow order-card">
+                      <div
+                        className="card order-card"
+                        style={{ backgroundColor: "#7998cd" }}
+                      >
                         <div className="card-block">
                           <div className="d-flex align-items-center justify-content-around">
-                            <FaMedal size="55px" />
+                            {/* <FaMedal size="55px" /> */}
                             <div className="d-flex flex-column justify-content-center">
                               <span className="h2 fw-bold">
                                 Alunos que mais emprestam
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {alunosMaisEmp1}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {alunosMaisEmp2}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {alunosMaisEmp3}
                               </span>
-                              <span className="h6 fw-bold">
-                                O Senhor dos Anéis - A sociedade do anel
+                              <span className="small fw-bold">
+                                {alunosMaisEmp4}
+                              </span>
+                              <span className="small fw-bold">
+                                {alunosMaisEmp5}
                               </span>
                             </div>
                           </div>
